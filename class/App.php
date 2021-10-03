1bbf4e2b9a0c8979c827d5fd858dd367db572348
christian-dale <jchaukvik@gmail.com>
2021-10-03 23:33:25 +0200

    Add function to load config file

--- class/App.php
+++ class/App.php
@@ -5,12 +5,21 @@ class App {
     public $lang = null;
     public $title = "";
     public $appname = "";
+    public $config = [];
     public $description = "";
     public $content = "";
 
     function __construct(Smarty $smarty, Lang $lang) {
         $this->smarty = $smarty;
         $this->lang = $lang;
+    }
+
+    function loadConfig() {
+        $config_file = file_get_contents("configs/config.json");
+        $this->config = json_decode($config_file, true);
+        $this->title = $this->config["title"];
+        $this->appname = $this->title;
+        $this->description = $this->config["description"];
     }
 
     function render() {
