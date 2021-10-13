e7275dda12bd10d057d353dbc88b1d6810302ac8
Jim Christian Haukvik <jimchristian@hotmail.no>
2021-10-13 15:01:21 +0200

    Refactor code in PageLoader

--- lib/class/PageLoader.php
+++ lib/class/PageLoader.php
@@ -25,17 +25,15 @@ class PageLoader {
     function loadRoutes(App $app, Router $router) {
         foreach ($this->nav_items as $item) {
             $router->add($item["url"], "get", function($res) use(&$app, $item) {
-                $item_plugin = $item["plugin"];
-                require_once("content/plugins/${item_plugin}/index.php");
-                $instance = new $item_plugin();
+                require_once("content/plugins/" . $item["plugin"] . "/index.php");
+                $instance = new $item["plugin"]();
                 $instance->init($res, $app, $item);
             });
         }
     }
 
     function loadPages() {
-        $json_file = file_get_contents("content/configs/pages.json");
-        $pages = json_decode($json_file, true);
+        $pages = App::loadJSON("content/configs/pages.json");
         $this->nav_items = array_map(fn($x) => array_merge($this->page_default, $x), $pages["pages"]);
     }
 
