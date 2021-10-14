3a49788327078614a92812ce83c007a4ef644a31
Jim Christian Haukvik <jimchristian@hotmail.no>
2021-10-14 23:39:09 +0200

    Add a new folder for default plugins

--- lib/class/PageLoader.php
+++ lib/class/PageLoader.php
@@ -24,11 +24,35 @@ class PageLoader {
             
     }
 
+    /**
+     * Get the directory of the plugin by name.
+     */
+
+    function getPlugin(string $plugin_name): string {
+        $plugin_path_content = "content/plugins/${plugin_name}/index.php";
+        $plugin_path_lib = "lib/plugins_default/${plugin_name}/index.php";
+
+        if (file_exists($plugin_path_content)) {
+            return $plugin_path_content;
+        } else if (file_exists($plugin_path_lib)) {
+            return $plugin_path_lib;
+        } else {
+            return "lib/plugins_default/DefaultHandler/index.php";
+        }
+    }
+
     function loadRoutes(\App\App $app, \App\Router $router) {
         foreach ($this->nav_items as $item) {
-            $router->add($item["url"], "get", function($res) use(&$app, $item) {
-                require_once("content/plugins/" . $item["plugin"] . "/index.php");
-                $instance = new $item["plugin"]();
+            $plugin = $item["plugin"];
+
+            // Use default plugin if specified plugin does not exist.
+            if (!file_exists("content/plugins/" . $item["plugin"] . "/index.php")) {
+                $plugin = "DefaultHandler";
+            }
+
+            $router->add($item["url"], "get", function($res) use(&$app, $item, $plugin) {
+                require_once($this->getPlugin($plugin));
+                $instance = new $plugin();
                 $instance->init($res, $app, $item);
             });
         }
@@ -36,6 +60,8 @@ class PageLoader {
 
     function loadPages() {
         $pages = \App\App::loadJSON("content/configs/pages.json");
+
+        // Add default properties to pages which do not have all properties.
         $this->nav_items = array_map(fn($x) => array_merge($this->page_default, $x), $pages["pages"]);
     }
 
