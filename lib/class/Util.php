457eaac179efc598f894a788a81b22bf85e020f5
christian-dale <jchaukvik@gmail.com>
2022-04-14 21:56:40 +0200

    Add Util getReqAttr and filterInput methods

--- lib/class/Util.php
+++ lib/class/Util.php
@@ -6,4 +6,12 @@ class Util {
     public static function issetAndTrue($var) {
         return isset($var) && $var == true;
     }
+
+    public static function getReqAttr(array $method, string $attr) {
+        return $method[$attr];
+    }
+
+    public static function filterInput($type, $var, $filter) {
+        return filter_input($type, $var, $filter);
+    }
 }
